bb8548bffc1a4b4dd71fe810da1ab45111cce3ef
stirante <stirantee@gmail.com>
2016-12-14 18:46:09 +0100

    Version 1.2.1

--- src/main/java/com/stirante/asem/Main.java
+++ src/main/java/com/stirante/asem/Main.java
@@ -24,7 +24,7 @@ import java.util.List;
  */
 public class Main extends Application {
 
-    public static final String VERSION = "1.2";
+    public static final String VERSION = "1.2.1";
     private static Stage stage;
     //Elements from layout
     @FXML
