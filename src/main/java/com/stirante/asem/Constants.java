6c587aff7339eaacce08634f9fac43ed051c3619
stirante <brzozowski.s.piotr@gmail.com>
2017-10-01 11:26:42 +0200

    Mark 1.8 version

--- src/main/java/com/stirante/asem/Constants.java
+++ src/main/java/com/stirante/asem/Constants.java
@@ -14,5 +14,5 @@ public class Constants {
     public static final String[] MNEMONIC_ARRAY = new String[]{"ACALL", "ADD", "ADDC", "AJMP", "ANL", "CJNE", "CLR", "CPL", "DA", "DEC", "DIV", "DJNZ", "INC", "JB", "JBC", "JC", "JMP", "JNB"
             , "JNC", "JNZ", "JZ", "LCALL", "LJMP", "MOV", "MOVC", "MOVX", "MUL", "NOP", "ORL", "POP", "PUSH", "RET", "RETI", "RL", "RLC", "RR", "RRC", "SETB", "SJMP", "SUBB", "SWAP", "XCH", "XCHD", "XRL"
     };
-    public static final double VERSION = 1.72;
+    public static final double VERSION = 1.8;
 }
